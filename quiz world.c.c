b9803dc7a80982719335b4afaf6accf9a1c481dc
Kshama6203 <104732917+Kshama6203@users.noreply.github.com>
2022-05-10 21:48:09 +0530

    Add files via upload

---
+++ quiz world.c.c
@@ -0,0 +1,416 @@
+//QUIZ WORLD
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   int count=0;
+   char a[10],b[10],c[10],d[15],e[15],f[15],g[15],h[15],i[15],j[15],k[15],l[15],m[15],n[15],o[15],p[15],q[15],r[15],s[15],t[15],u[15],v[15],w[15],x[15],y[15],z[15],A[15],B[15],C[15],D[15],E[15],F[15],G[15],H[15],I[15],J[15];
+   printf("Welcome To The World Of Quiz\n");
+   printf("For Each Correct Answer You Will Be Awarded:+1\n");
+   printf("For Each Wrong Answer You Will Be Awarded :+0\n");
+   printf("Enter Your Capitals Name In Block Letters Only\n");
+   printf("LET'S BEGIN:\n");
+   printf("Capital Of Andhra Pradesh Is:\n");
+   scanf("%s",a);
+   if(strcmp(a,"AMRAVATI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Arunachal Pradesh Is:\n");
+   scanf("%s",b);
+   if(strcmp(b,"ITANAGAR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Assam Is:\n");
+   scanf("%s",c);
+   if(strcmp(c,"DISPUR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Bihar Is:\n");
+   scanf("%s",d);
+   if(strcmp(d,"PATNA"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Chattisgarh Is:\n");
+   scanf("%s",e);
+   if(strcmp(e,"RAIPUR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Goa Is:\n");
+   scanf("%s",f);
+   if(strcmp(f,"PANAJI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Gujarat Is:\n");
+   scanf("%s",g);
+   if(strcmp(g,"GANDHINAGAR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Haryana Is:\n");
+   scanf("%s",h);
+   if(strcmp(h,"CHANDIGARH"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Himachal Pradesh Is:\n");
+   scanf("%s",i);
+   if(strcmp(i,"SHIMLA"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Jharkhand Is:\n");
+   scanf("%s",j);
+   if(strcmp(j,"RANCHI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Karnataka Is:\n");
+   scanf("%s",k);
+   if(strcmp(k,"BENGALURU"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Kerala Is:\n");
+   scanf("%s",l);
+   if(strcmp(l,"THIRUVANANTHAPURAM"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Madhya Pradesh Is:\n");
+   scanf("%s",m);
+   if(strcmp(m,"BHOPAL"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Maharashtra  Is:\n");
+   scanf("%s",n);
+   if(strcmp(n,"MUMBAI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Manipur Is:\n");
+   scanf("%s",o);
+   if(strcmp(o,"IMPHAL"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Meghalaya Is:\n");
+   scanf("%s",p);
+   if(strcmp(p,"SHILLONG"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Mizoram Is:\n");
+   scanf("%s",q);
+   if(strcmp(q,"AIZAWL"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Nagaland Is:\n");
+   scanf("%s",r);
+   if(strcmp(r,"KOHIMA"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Odissa Is:\n");
+   scanf("%s",s);
+   if(strcmp(s,"BHUBANESWAR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Punjab Is:\n");
+   scanf("%s",t);
+   if(strcmp(t,"CHANDIGARH"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Rajasthan  Is:\n");
+   scanf("%s",u);
+   if(strcmp(u,"JAIPUR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Sikkim Is:\n");
+   scanf("%s",v);
+   if(strcmp(v,"GANGTOK"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Tamil Nadu Is:\n");
+   scanf("%s",w);
+   if(strcmp(w,"CHENNAI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Telangana Is:\n");
+   scanf("%s",x);
+   if(strcmp(x,"HYDERABAD"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Tripura Is:\n");
+   scanf("%s",y);
+   if(strcmp(y,"AGARTALA"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Uttar Pradesh Is:\n");
+   scanf("%s",z);
+   if(strcmp(z,"LUCKNOW"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Uttarakhand Is:\n");
+   scanf("%s",A);
+   if(strcmp(A,"DEHRADUN || GAIRSAIN"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of West Bengal Is:\n");
+   scanf("%s",B);
+   if(strcmp(B,"KOLKATA"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Enter the capital names of Indian Union Territories:\n");
+   printf("Capital Of Andaman And Nicobar Island Is:\n");
+   scanf("%s",C);
+   if(strcmp(C,"PORT BLAIR"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Chandigarh Is:\n");
+   scanf("%s",D);
+   if(strcmp(D,"CHANDIGARH"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Dadra And Nagar Haveli And Daman And Diu Is:\n");
+   scanf("%s",E);
+   if(strcmp(E,"DAMAN"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of DELHI Is:\n");
+   scanf("%s",F);
+   if(strcmp(F,"NEW DELHI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Jammu And Kashmir Is:\n");
+   scanf("%s",G);
+   if(strcmp(G,"SRINAGAR || JAMMU"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Lakshadweep Is:\n");
+   scanf("%s",H);
+   if(strcmp(H,"KAVARATTI"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Puducherry Is:\n");
+   scanf("%s",I);
+   if(strcmp(I,"PONDICHERRY"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("Capital Of Ladakh Is:\n");
+   scanf("%s",J);
+   if(strcmp(J,"LEH"))
+   {
+    printf("Your Answer Is Wrong\n");
+   }
+   else
+   {
+     count++;
+     printf("Your Answer Is Correct\n");
+   }
+   printf("%d",count);
+   return(0);
+}
+
+
+
+
